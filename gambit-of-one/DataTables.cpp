afee0860c38e681fac14ec1fb1865d5d588d215a
Dylan Purvis <dylan.purvis14@ncf.edu>
2015-05-17 23:03:11 -0400

    Started on creatures.

---
+++ gambit-of-one/DataTables.cpp
@@ -0,0 +1,84 @@
+#include "DataTables.hpp"
+#include "Aircraft.hpp"
+#include "Projectile.hpp"
+#include "Pickup.hpp"
+
+
+// For std::bind() placeholders _1, _2, ...
+using namespace std::placeholders;
+
+std::vector<CreatureData> initializeCreatureData()
+{
+	std::vector<CreatureData> data(Creature::TypeCount);
+
+	data[Creature::Rat].hitpoints = 100;
+	data[Creature::Rat].speed = 100;
+	data[Creature::Rat].damage = 5;
+	//aggrodistance-> distance from player at which an idle enemy becomes aggressive
+	data[Creature::Rat].aggrodistance = tba;
+	data[Creature::Rat].texture = Textures::Rat;
+
+
+	/*data[Aircraft::Eagle].hitpoints = 100;
+	data[Aircraft::Eagle].speed = 200.f;
+	data[Aircraft::Eagle].fireInterval = sf::seconds(1);
+	data[Aircraft::Eagle].texture = Textures::Eagle;
+
+	data[Aircraft::Raptor].hitpoints = 20;
+	data[Aircraft::Raptor].speed = 80.f;
+	data[Aircraft::Raptor].texture = Textures::Raptor;
+	data[Aircraft::Raptor].directions.push_back(Direction(+45.f, 80.f));
+	data[Aircraft::Raptor].directions.push_back(Direction(-45.f, 160.f));
+	data[Aircraft::Raptor].directions.push_back(Direction(+45.f, 80.f));
+	data[Aircraft::Raptor].fireInterval = sf::Time::Zero;
+
+	data[Aircraft::Avenger].hitpoints = 40;
+	data[Aircraft::Avenger].speed = 50.f;
+	data[Aircraft::Avenger].texture = Textures::Avenger;
+	data[Aircraft::Avenger].directions.push_back(Direction(+45.f, 50.f));
+	data[Aircraft::Avenger].directions.push_back(Direction(0.f, 50.f));
+	data[Aircraft::Avenger].directions.push_back(Direction(-45.f, 100.f));
+	data[Aircraft::Avenger].directions.push_back(Direction(0.f, 50.f));
+	data[Aircraft::Avenger].directions.push_back(Direction(+45.f, 50.f));
+	data[Aircraft::Avenger].fireInterval = sf::seconds(2);*/
+
+	return data;
+}
+
+std::vector<ProjectileData> initializeProjectileData()
+{
+	std::vector<ProjectileData> data(Projectile::TypeCount);
+
+	data[Projectile::AlliedBullet].damage = 10;
+	data[Projectile::AlliedBullet].speed = 300.f;
+	data[Projectile::AlliedBullet].texture = Textures::Bullet;
+
+	data[Projectile::EnemyBullet].damage = 10;
+	data[Projectile::EnemyBullet].speed = 300.f;
+	data[Projectile::EnemyBullet].texture = Textures::Bullet;
+
+	data[Projectile::Missile].damage = 200;
+	data[Projectile::Missile].speed = 150.f;
+	data[Projectile::Missile].texture = Textures::Missile;
+
+	return data;
+}
+
+std::vector<PickupData> initializePickupData()
+{
+	std::vector<PickupData> data(Pickup::TypeCount);
+
+	data[Pickup::HealthRefill].texture = Textures::HealthRefill;
+	data[Pickup::HealthRefill].action = [](Aircraft& a) { a.repair(25); };
+
+	data[Pickup::MissileRefill].texture = Textures::MissileRefill;
+	data[Pickup::MissileRefill].action = std::bind(&Aircraft::collectMissiles, _1, 3);
+
+	data[Pickup::FireSpread].texture = Textures::FireSpread;
+	data[Pickup::FireSpread].action = std::bind(&Aircraft::increaseSpread, _1);
+
+	data[Pickup::FireRate].texture = Textures::FireRate;
+	data[Pickup::FireRate].action = std::bind(&Aircraft::increaseFireRate, _1);
+
+	return data;
+}
---
+++ gambit-of-one/DataTables.hpp
@@ -0,0 +1,54 @@
+#ifndef DATATABLES_HPP
+#define DATATABLES_HPP
+
+#include "ResourceIdentifiers.hpp"
+
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/Color.hpp>
+
+#include <vector>
+#include <functional>
+
+
+class Creature;
+
+struct Direction
+{
+	Direction(float angle, float distance)
+		: angle(angle)
+		, distance(distance)
+	{
+	}
+
+	float angle;
+	float distance;
+};
+
+struct CreatureData
+{
+	int								hitpoints;
+	float							speed;
+	Textures::ID					texture;
+	sf::Time						fireInterval;
+	std::vector<Direction>			directions;
+};
+
+struct ProjectileData
+{
+	int								damage;
+	float							speed;
+	Textures::ID					texture;
+};
+
+struct PickupData
+{
+	std::function<void(Creature&)>	action;
+	Textures::ID					texture;
+};
+
+
+std::vector<CreatureData>	initializeAircraftData();
+std::vector<ProjectileData>	initializeProjectileData();
+std::vector<PickupData>		initializePickupData();
+
+#endif // DATATABLES_HPP
